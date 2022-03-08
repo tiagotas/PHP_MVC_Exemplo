1533e656c9affa8ab23d95eabd9df31028fd9a8c
Tiago Silva <frost.tiago@gmail.com>
2022-03-08 13:41:51 -0300

    Adicao do repositorio

---
+++ App/index.php
@@ -0,0 +1,56 @@
+<?php
+
+$uri_parse = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+//echo $uri_parse;
+//echo "<hr />";
+
+include 'Controller/PessoaController.php';
+
+switch($uri_parse)
+{
+    case '/pessoa':
+        PessoaController::index();
+    break;
+
+    case '/pessoa/form':
+        PessoaController::form();
+    break;
+
+    case '/pessoa/save':
+        PessoaController::save();
+    break;
+    
+    
+    
+    case '/formulario':
+        include 'View/udy.php';
+    break;
+
+    case '/processa':
+        echo "vai pegar o que o usuário digitou <br />";
+        echo $_POST['nome'];
+        echo "<br />";
+        var_dump($_POST);
+    break;
+
+    case '/produto':
+        echo "listar produtos";
+    break;
+
+    case '/produto/ver':
+        echo "ver detalhes de produto";
+    break;
+
+    case '/produto/delete':
+        echo "remover produto";
+    break;
+
+    case '/produto/salvar':
+        echo "salva no banco de dados";
+    break;
+
+    default:
+        echo "erro 404";
+    break;
+}
